106a9cca9f9b6384fc19aefe0ad38e286cd5ab0c
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-12-03 09:09:08 +0100

    Add flask_restxpip package

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_restx import Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
