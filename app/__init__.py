ea52fb9607e33e14b1b5d8493f050932ac767f56
Quentin Neves <neves.quentin@gmail.com>
2021-12-10 14:14:27 +0100

    Test json, array and objects in python

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_restx import Api
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World mdr lol!</p>"
+
+@app.route("/supermama")
+def super():
+    hihi = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
+    return (str(hihi))
+    #return json.dumps(hihi)
