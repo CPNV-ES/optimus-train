325cb9c47097f0814c72b4a47f0deb40031923e4
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-12-17 08:18:09 +0100

    remove dev function

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,6 @@ from flask_restx import Api
 app = Flask(__name__)
 api = Api(app)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 @app.route("/trains")
 def trains():
     return "<p>Trains's section</p>"
