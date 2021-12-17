ba269d2e3f6ceeda912c1393169d1fac5d6be0f1
AlexandrePhilibertCPNV <alexandre.philibert@cpnv.ch>
2021-12-17 09:17:30 +0100

    Add trains request to SBB api

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restx import Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
+
 @app.route("/trains")
 def trains():
-    value = {
-        "company": "compagny1",
-        "name": "train1",
-        "weight": "999"
-    }
-    return value
+    response = requests.get(
+        "https://data.sbb.ch/api/records/1.0/search/?dataset=jahresformation&q=&facet=zug&facet=debicode&facet=zugart&facet=bhf_von&facet=bhf_bis&facet=umlauf&facet=block_bezeichnung&facet=beginnfahrplanperiode")
+
+    json = response.json()
+
+    return jsonify(json['records'])
+
 
 @app.route("/schedules")
 def schedules():
